82e51213e7a538ec47042b971c478596174f9d44
Rudeil <Rudeil@yandex.ru>
2022-12-12 06:27:22 +0300

    Только прокинуть запросы и поправить UI

--- app/src/main/java/com/example/hakatonfinaljava/Client.java
+++ app/src/main/java/com/example/hakatonfinaljava/Client.java
@@ -2,13 +2,35 @@ package com.example.hakatonfinaljava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 
 public class Client extends AppCompatActivity {
-
+    private String macAddrUser;
+    private String macAddrWork = " ";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client);
+        WifiManager wifiManager = (WifiManager) this.getApplicationContext().getSystemService(
+                Context.WIFI_SERVICE);
+        WifiInfo wifiInf = wifiManager.getConnectionInfo();
+       macAddrUser = wifiInf.getBSSID().toString();
+       CheckMAC(macAddrUser);
+
     }
+    public void CheckMAC(String macAddrUser) {
+        //if (macAddrUser != null $$ macAddrUser !="02:00:00:00:00:00")
+        if (macAddrUser == "02:00:00:00:00:00"){
+            //TODO Показать popup, что нужно включить Геолокаци и дать разрешение + запросить разрешение
+        }
+        while (macAddrUser == macAddrWork){
+            //TODO отправлять запрос каждые 6 минут
+        }
+
+
+        }
+
 }
